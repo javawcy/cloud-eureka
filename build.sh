8b2213b4e00bcf6fc5d7eab6c0e3913bbeb3b3c6
Chongyu <javawcy@outlook.com>
2020-09-19 18:28:21 +0800

    fix: reconfig server

--- build.sh
+++ build.sh
@@ -2,19 +2,8 @@ echo "====eureka server====="
 
 mvn clean install -Dmaven.test.skip=true
 
-#read -r -p "chose server [1/2/3]: " server
-
 docker build -f docker/Dockerfile -t eureka:latest .
-docker rm -f $(docker ps | grep 'eureka*' | awk '{print$1}')
-
-#port=8080
-#case $server in
-#  1) port=9991
-#  ;;
-#  2) port=9992
-#  ;;
-#  3) port=9993
-#esac
+docker rm -f $(docker ps -a | grep 'eureka*' | awk '{print$1}')
 
 docker run -d -p 9991:8080 --name eureka1 --hostname eureka1 --network cloud-demo eureka:latest
 docker run -d -p 9992:8080 --name eureka2 --hostname eureka2 --network cloud-demo eureka:latest
