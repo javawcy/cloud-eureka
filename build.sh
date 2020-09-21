0447b27aed03d69bf04f3364f3efb114acb8cf71
Chongyu <javawcy@outlook.com>
2020-09-21 16:06:34 +0800

    fix: change node

--- build.sh
+++ build.sh
@@ -2,9 +2,10 @@ echo "====eureka server====="
 
 mvn clean install -Dmaven.test.skip=true
 
-docker build -f docker/Dockerfile -t eureka:latest .
-docker rm -f $(docker ps -a | grep 'eureka*' | awk '{print$1}')
+docker build -f docker/Dockerfile -t 192.168.10.124:8889/public/eureka:latest .
+docker push 192.168.10.124:8889/public/eureka:latest
+docker rmi 192.168.10.124:8889/public/eureka:latest
 
-docker run -d -p 9991:8080 --name eureka1 --hostname eureka1 --network cloud-demo eureka:latest
-docker run -d -p 9992:8080 --name eureka2 --hostname eureka2 --network cloud-demo eureka:latest
-docker run -d -p 9993:8080 --name eureka3 --hostname eureka3 --network cloud-demo eureka:latest
+#docker run -d -p 9991:8080 --name eureka1 --hostname eureka1 --network cloud-demo 192.168.10.124:8889/public/eureka:latest
+#docker run -d -p 9992:8080 --name eureka2 --hostname eureka2 --network cloud-demo 192.168.10.124:8889/public/eureka:latest
+#docker run -d -p 9993:8080 --name eureka3 --hostname eureka3 --network cloud-demo 192.168.10.124:8889/public/eureka:latest
